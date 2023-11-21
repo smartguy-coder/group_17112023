b25a53a7f12e05600d08dd64082f0e8a65c53655
vasylkartychak <w.i.k.mailua@gmail.com>
2023-11-21 21:30:30 +0200

    some numbers

---
+++ numbers_theory.py
@@ -0,0 +1,27 @@
+# price = 15_000
+# quantity = 101
+#
+# total_cars_cost = price * quantity
+# print(total_cars_cost)
+# print(f'{total_cars_cost=}')
+#
+# # insurance_rate = 0.05
+# insurance_rate = 0.051 / 100
+#
+# print(f'{insurance_rate=}')
+# ins_per_car = insurance_rate * price
+# print(ins_per_car)
+#
+# strange_number = 0.1 + 1.403333
+# print(strange_number)
+# less_strange_number = round(strange_number, 3)
+# print(round(strange_number, 3))
+# print(less_strange_number)
+
+print(101 % 50)
+print(101 / 50)
+print(100 % 50)
+
+print(100 // 33)
+
+print(2**2)
